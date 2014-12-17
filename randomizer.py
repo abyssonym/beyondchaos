41fd06bea7061349a8a8f13bc39148811d5ff11e
Anonymous <none>
2014-12-17 03:28:44 -0800

    update version 35

--- randomizer.py
+++ randomizer.py
@@ -29,7 +29,7 @@ from locationrandomizer import Zone, EntranceSet, get_locations
 from towerrandomizer import randomize_tower
 
 
-VERSION = "34"
+VERSION = "35"
 VERBOSE = False
 flags = None
 sourcefile, outfile = None, None

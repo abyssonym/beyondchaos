1a1da6cffb299982f81125c201e0960dc498bf03
Anonymous <none>
2015-03-17 11:51:56 -0700

    add some simple tests to ensure that the randomizer doesn't crash on certain flag combinations

---
+++ unittests.sh
@@ -0,0 +1,48 @@
+#TIME=`date +%s`
+TIME=1426615861
+rm program.*.rom
+rm program.*.txt
+set -e
+yes | python randomizer.py program.rom .owzbmciqetulnfspdah.$TIME test
+yes | python randomizer.py program.rom .wzbmciqetulnfspdah.$TIME test
+yes | python randomizer.py program.rom .ozbmciqetulnfspdah.$TIME test
+yes | python randomizer.py program.rom .owbmciqetulnfspdah.$TIME test
+yes | python randomizer.py program.rom .owzmciqetulnfspdah.$TIME test
+yes | python randomizer.py program.rom .owzbciqetulnfspdah.$TIME test
+yes | python randomizer.py program.rom .owzbmiqetulnfspdah.$TIME test
+yes | python randomizer.py program.rom .owzbmcqetulnfspdah.$TIME test
+yes | python randomizer.py program.rom .owzbmcietulnfspdah.$TIME test
+yes | python randomizer.py program.rom .owzbmciqtulnfspdah.$TIME test
+yes | python randomizer.py program.rom .owzbmciqeulnfspdah.$TIME test
+yes | python randomizer.py program.rom .owzbmciqetlnfspdah.$TIME test
+yes | python randomizer.py program.rom .owzbmciqetunfspdah.$TIME test
+yes | python randomizer.py program.rom .owzbmciqetulfspdah.$TIME test
+yes | python randomizer.py program.rom .owzbmciqetulnspdah.$TIME test
+yes | python randomizer.py program.rom .owzbmciqetulnfpdah.$TIME test
+yes | python randomizer.py program.rom .owzbmciqetulnfsdah.$TIME test
+yes | python randomizer.py program.rom .owzbmciqetulnfspah.$TIME test
+yes | python randomizer.py program.rom .owzbmciqetulnfspdh.$TIME test
+yes | python randomizer.py program.rom .owzbmciqetulnfspda.$TIME test
+yes | python randomizer.py program.rom .o.$TIME test
+yes | python randomizer.py program.rom .w.$TIME test
+yes | python randomizer.py program.rom .z.$TIME test
+yes | python randomizer.py program.rom .b.$TIME test
+yes | python randomizer.py program.rom .m.$TIME test
+yes | python randomizer.py program.rom .c.$TIME test
+yes | python randomizer.py program.rom .i.$TIME test
+yes | python randomizer.py program.rom .q.$TIME test
+yes | python randomizer.py program.rom .e.$TIME test
+yes | python randomizer.py program.rom .t.$TIME test
+yes | python randomizer.py program.rom .u.$TIME test
+yes | python randomizer.py program.rom .l.$TIME test
+yes | python randomizer.py program.rom .n.$TIME test
+yes | python randomizer.py program.rom .f.$TIME test
+yes | python randomizer.py program.rom .s.$TIME test
+yes | python randomizer.py program.rom .p.$TIME test
+yes | python randomizer.py program.rom .d.$TIME test
+yes | python randomizer.py program.rom .a.$TIME test
+yes | python randomizer.py program.rom .h.$TIME test
+yes | python randomizer.py program.rom ._.$TIME test
+echo "TEST RUNS SUCCESSFUL"
+rm program.*.rom
+rm program.*.txt

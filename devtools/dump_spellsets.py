4190481e5936dd86b735512cabc06b2d00b0cdde
Glenn Matthews <glenn@e-dad.net>
2017-05-14 15:51:32 -0400

    dump_spellsets developer tool

---
+++ devtools/dump_spellsets.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""Usage: dump_spellsets.py <ROM file>
+
+Dump the spellsets that skillrandomizer uses for generating new PC commands
+(R-Nuke, 3xBeast, W-Sword, etc.)
+"""
+
+import os
+import sys
+import textwrap
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from skillrandomizer import SpellBlock, get_spellsets, spelldict
+
+sourcefile = sys.argv[1]
+
+print("Loading skills from '{0}'".format(sourcefile))
+
+all_spells = [SpellBlock(i, sourcefile) for i in xrange(0xFF)]
+
+for s in all_spells:
+    spelldict[s.spellid] = s
+
+spellsets = get_spellsets(all_spells)
+
+for name in sorted(spellsets.keys()):
+    desc, spells = spellsets[name]
+    print("\n\n{0}\n==========".format(name))
+    print("Set members are described as '{0}'".format(desc))
+    print(textwrap.fill("".join("{0:13}".format(s.name) for s in spells), 76))

d33d9964b0827e37510498113883a76e0a7d288a
subtractionsoup <30951058+subtractionsoup@users.noreply.github.com>
2018-10-26 22:52:26 -0700

    wip

--- randomizer.py
+++ randomizer.py
@@ -4112,7 +4112,7 @@ def manage_map_names():
         write_multi(fout, pointer, length=2)
 
 
-def manage_wor():
+def manage_wor_skip(wor_free_char=0xB):
     characters = get_characters()
 
     # jump to FC end cutscene for more space
@@ -4279,6 +4279,8 @@ def manage_wor():
         line = line.strip().split(',')
         setbit = int(line[1], 16)  # if 1, set the bit from the txt file
         bit = line[0]  # the bit to set/clear from the txt file
+        if bit == "2FB":
+            bit = "2F" + str(wor_free_char)
         firstbyte = 0xD1 + int(bit[0:1], 16) * 2 - setbit
         lastbyte = int(bit[1:], 16)
         wor_sub2.bytestring += [firstbyte, lastbyte]
@@ -5668,47 +5670,75 @@ def manage_dances():
         log(dancestr, "dances")
 
 class WoRRecruitInfo(object):
-    def __init__(self, event_pointers, recruited_bit_pointer, location_npcs, prequisite=None):
+    def __init__(self, event_pointers, recruited_bit_pointers, location_npcs, prerequisite=None, special=None):
         self.event_pointers = event_pointers
-        self.recruited_bit_pointer = recruited_bit_pointer
+        self.recruited_bit_pointers = recruited_bit_pointers
         self.location_npcs = location_npcs
+        self.prerequisite = prerequisite
+        self.special = special
 
     def write_data(self, fout):
-        assert(self.char_id)
+        assert(self.char_id is not None)
         for event_pointer in self.event_pointers:
             fout.seek(event_pointer)
             fout.write(chr(self.char_id))
-        fout.seek(self.recruited_bit_pointer)
-        fout.write(chr(0xf0 + self.char_id))
-        for location_id, npc_id in location_npcs:
+        for recruited_bit_pointer in self.recruited_bit_pointers:
+            fout.seek(recruited_bit_pointer)
+            fout.write(chr(0xf0 + self.char_id))
+        for location_id, npc_id in self.location_npcs:
             location = get_location(location_id)
             npc = location.npcs[npc_id]
             npc.graphics = self.char_id
             npc.palette = get_character(self.char_id).palette
+        if self.special:
+            self.special(self.char_id)
+
+def gau_recruit(char_id):
+    gau_recruit_sub = Substitution()
+    gau_recruit_sub.set_location(0xA5324)
+    gau_recruit_sub.bytestring = [0xD5, 0xFB]
+    gau_recruit_sub.write(fout)
+
+    gau_recruit_sub.set_location(0xA5310 + 2 * char_id - (2 if char_id > 6 else 0))
+    gau_recruit_sub.bytestring = [0xD4, 0xF0 + char_id]
+    gau_recruit_sub.write(fout)
 
 def manage_wor_recruitment():
-    candidates = [0x01, 0x02, 0x05, 0x07, 0x08]
+    candidates = [0x00, 0x01, 0x02, 0x05, 0x07, 0x08, 0x0B]
     recruit_info = [
         # Phoenix Cave / Locke
-        WoRRecruitInfo([0xc2c48, 0xc2c51, 0xc2c91, 0xc2c9d, 0xc2caf, 0xc2cb8, 0xc2cc5, 0xc2cca, 0xc2cd8, 0xc2ce3, 0xc2ce9, 0xc2cee, 0xc2cf4, 0xc2cfa, 0xc2d0b, 0xc2d33, 0xc2e32, 0xc2e80, 0xc2e86, 0xc2e8b, 0xc2e91, 0xc2ea5, 0xc2eb1, 0xc2ec4, 0xc2f0b, 0xc2fe1, 0xc3102, 0xc3106, 0xc3117, 0xc311d, 0xc3124, 0xc3134, 0xc313d, 0xc3163, 0xc3183, 0xc3185, 0xc3189, 0xc318b, 0xc318e, 0xc3191, 0xc3197, 0xc31c7, 0xc31cb, 0xc31e2, 0xc31e8, 0xc31ed, 0xc31f2, 0xc31f8, 0xc3210, 0xc3215, 0xc312d, 0xc3229, 0xc322f, 0xc3235, 0xc323b, 0xc3244, 0xc324a, 0xc324f, 0xc3258, 0xc326a], 0xc3195, [(0x139, 0)])
+        WoRRecruitInfo([0xc2c48, 0xc2c51, 0xc2c91, 0xc2c9d, 0xc2caf, 0xc2cb8, 0xc2cc5, 0xc2cca, 0xc2cd8, 0xc2ce3, 0xc2ce9, 0xc2cee, 0xc2cf4, 0xc2cfa, 0xc2d0b, 0xc2d33, 0xc2e32, 0xc2e80, 0xc2e86, 0xc2e8b, 0xc2e91, 0xc2ea5, 0xc2eb1, 0xc2ec4, 0xc2f0b, 0xc2fe1, 0xc3102, 0xc3106, 0xc3117, 0xc311d, 0xc3124, 0xc3134, 0xc313d, 0xc3163, 0xc3183, 0xc3185, 0xc3189, 0xc318b, 0xc318e, 0xc3191, 0xc3197, 0xc31c7, 0xc31cb, 0xc31e2, 0xc31e8, 0xc31ed, 0xc31f2, 0xc31f8, 0xc3210, 0xc3215, 0xc312d, 0xc3229, 0xc322f, 0xc3235, 0xc323b, 0xc3244, 0xc324a, 0xc324f, 0xc3258, 0xc326a], [0xc3195], [(0x139, 0)]),
         # Mt. Zozo / Cyan
-        WoRRecruitInfo([0xc429c, 0xc429e, 0xc42a2, 0xc42a4, 0xc42a7, 0xc42aa], 0xc42ad, [(0xb4, 8), (0xb5, 2)])
+        WoRRecruitInfo([0xc429c, 0xc429e, 0xc42a2, 0xc42a4, 0xc42a7, 0xc42aa], [0xc42ae], [(0xb4, 8), (0xb5, 2)]),
         # Collapsing House / Sabin
-        WoRRecruitInfo([0xc5aa8, 0xc5aaa, 0xc5aae, 0xc5ab0, 0xc5ab3, 0xc5ab6], 0xc5aba, [(0x131, 1)]),
+        WoRRecruitInfo([0xc5aa8, 0xc5aaa, 0xc5aae, 0xc5ab0, 0xc5ab3, 0xc5ab6], [0xc5aba], [(0x131, 1)]),
         # Fanatics' Tower / Strago
-        WoRRecruitInfo([0xc5418, 0xc541a, 0xc541e, 0xc5420, 0xc5423, 0xc5426], 0xc542a, [(0x16a, 1)], prerequisite=[0x08])
+        WoRRecruitInfo([0xc5418, 0xc541a, 0xc541e, 0xc5420, 0xc5423, 0xc5426], [0xc542a], [(0x16a, 3)], prerequisite=[0x08]),
         # Owzer's House / Relm
-        WoRRecruitInfo([0xb4e09, 0xb4e0b, 0xb4e0f, 0xb4e11, 0xb4e14, 0xb4e17], 0xb4e1b [(0x161, 3), (0x15d, 21), (0xd0, 3)])
+        WoRRecruitInfo([0xb4e09, 0xb4e0b, 0xb4e0f, 0xb4e11, 0xb4e14, 0xb4e17], [0xb4e1b], [(0x161, 3), (0x15d, 21), (0xd0, 3)]),
+        # Mobliz / Terra
+        WoRRecruitInfo([0xc49d1, 0xc49d3, 0xc49da, 0xc49de, 0xc49e2, 0xc4a01, 0xc4a03, 0xc4a0c, 0xc4a0d, 0xc4a2b, 0xc4a37, 0xc4a3a, 0xc4a43, 0xc4a79, 0xc4a7b, 0xc4ccf, 0xc4cd1, 0xc4cd5, 0xc4cd7, 0xc4cdb, 0xc4cde, 0xc4ce1, 0xc4ce5, 0xc4cf4, 0xc4cf6, 0xc5040, 0xc5042, 0xc5048, 0xc504a, 0xc504d, 0xc5050], 
+        [0xc4cd9, 0xc4cfa, 0xc5046],
+        [(0x09A, 1), (0x09A, 2), (0x096, 0), (0x09E, 13)]),
+        WoRRecruitInfo([], [], [], special=gau_recruit)
     ]
     
-    restricted_info = [info for info in recruit_info if info.prequisite]
-    unrestricted_info = [info for info in recruit_info if not info.prequisite]
+    restricted_info = [info for info in recruit_info if info.prerequisite]
+    unrestricted_info = [info for info in recruit_info if not info.prerequisite]
     recruit_info = restricted_info + unrestricted_info
+    wor_free_char = 0xB  # gau
     for info in recruit_info:
-        candidate = random.choice(candidates)
+        valid_candidates = candidates
+        if info.prerequisite:
+            valid_candidates = [c for c in candidates if c not in info.prerequisite]
+        candidate = random.choice(valid_candidates)
         candidates.remove(candidate)
         info.char_id = candidate
+        if info.special == gau_recruit:
+            wor_free_char = candidate
         info.write_data(fout)
+        
+    return wor_free_char
     #Dialogue 08a7, 08a8, 08a9, 08aa, 08ab, 08ac, 08ad, 08ae, 08b1
     #Sabin
     #Event CC5aa8, cc5aaa, cc5aae, cc5ab0 cc5ab3 cc5ab6
@@ -5739,6 +5769,12 @@ def manage_wor_recruitment():
     #event b4e09, b4e0b, b4e0f, b4e11, b4e14, b4e17,
     #eventbit b4e1b
     #map 161, npc 3, map 15d, npc 21, map 0d0 npc 3
+    
+    #Terra
+    # dialogue 08cf, 08d1, 08d2, 08d3, 08d4, 08d5, 08d6, 08d7, 08d8, 08d9, 08db, 08dc, 08dd, 08df, 08e0, 08e5, 08eb, 08ec, 08f0, 08f6, 08f7, 08f8, 08f9, 08fa, 08fb, 08fc, 08fe, 0900, 0903, 0906, 090b, *more for kefka's tower w/o terra
+    # event c49d1, c49d3, c49da, c49de, c49e2, c4a01, c4a03, c4a2b, c4a37, c4a3a, c4a43, c4a79, c4a7b, c4ccf, c4cd1, c4cd5, c4cd7, c4cdb, c4cde, c4ce1, c4ce5, c4cf4, c4cf6, c5040, c5042, c5048, c504a, c504d, c5050, *more for kefka's tower w/o terra
+    #eventbit c4cd9, c4cfa, c5045
+    #map 09A npc 1, 09A npc 2, 096 npc 0, 09E npc 13, *0b7 npc 0, *0b8 npc 0
     
     #Gau
     #if 'o' in flags or 'w' in flags or 't' in flags:
@@ -5824,6 +5860,7 @@ a   Organize rages alphabetically (default)
 h   Organize rages by highest level first
 g   Randomize dances
 k   Randomize the clock in Zozo
+r   Randomize character locations in the world of ruin.
 -   Use all flags EXCEPT the ones listed'''
 
     if len(args) > 2:
@@ -6224,8 +6261,12 @@ k   Randomize the clock in Zozo
         create_dimensional_vortex()
     reseed()
 
+    wor_free_char = 0xB  # gau
+    if 'r' in flags and 'ancientcave' not in activated_codes:
+        wor_free_char = manage_wor_recruitment()
+
     if 'worringtriad' in activated_codes:
-        manage_wor()
+        manage_wor_skip(wor_free_char)
     reseed()
 
     if 'k' in flags:
@@ -6237,8 +6278,6 @@ k   Randomize the clock in Zozo
             manage_dances()
     reseed()
 
-    manage_wor_recruitment()
-    reseed()
     # ----- NO MORE RANDOMNESS PAST THIS LINE -----
     write_all_locations_misc()
     for fs in fsets:

841544723664da147153e0c4125366ad0b77ca25
GreenKnight5 <greenknight5.jf@gmail.com>
2019-09-01 00:11:08 -0400

    flag selection works and flag files added to project

--- GUI.py
+++ GUI.py
@@ -1,21 +1,21 @@
+import re
 import sys
 
 from PyQt5 import QtGui, Qt
 from PyQt5.QtWidgets import QPushButton, QCheckBox, QWidget, QVBoxLayout, QLabel, QGroupBox, QHBoxLayout, QLineEdit, \
     QRadioButton, QGridLayout, QComboBox, QFileDialog, QApplication, QTabWidget
-from PyQt5.uic.properties import QtCore
 
 
 class FlagButton(QPushButton):
     def __init__(self, text, value):
-        super(FlagButton,self).__init__()
+        super(FlagButton, self).__init__()
         self.setText(text)
         self.value = value
 
 
 class FlagCheckBox(QCheckBox):
     def __init__(self, text, value):
-        super(FlagCheckBox,self).__init__()
+        super(FlagCheckBox, self).__init__()
         self.setText(text)
         self.value = value
 
@@ -25,14 +25,19 @@ class Window(QWidget):
     def __init__(self):
         super().__init__()
 
+        # window geometry info
         self.title = "Beyond Chaos Randomizer"
         self.left = 200
         self.top = 200
         self.width = 650
         self.height = 600
 
+        # values to be sent to randomizer
         self.romText = ""
+        self.mode = ""
+        self.flags = ""
 
+        # dictionaries to hold flag data
         self.aesthetic = {}
         self.experimental = {}
         self.gamebreaking = {}
@@ -40,12 +45,14 @@ class Window(QWidget):
         self.minor = {}
         self.simple = {}
 
+        # create window using geometry data
         self.InitWindow()
 
     def InitWindow(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
+        self.loadCodes()
         self.CreateLayout()
 
         self.show()
@@ -96,8 +103,6 @@ class Window(QWidget):
 
     # Middle groupbox of sub-groupboxes. Consists of left section (game mode selection)
     #   and right section (flag selection -> tab-sorted)
-    # Flag selection is loop-driven generation with data read from text files. The option
-    #   to save flag selections is available
     def GroupBoxTwoLayout(self):
         groupBoxTwo = QGroupBox()
         middleHBox = QHBoxLayout()
@@ -160,27 +165,85 @@ class Window(QWidget):
         tabs.addTab(tab5, "Experimental")
         tabs.addTab(tab6, "Gamebreaking")
 
+        # ----------- tab1 -------------------------
         flagGrid = QGridLayout()
-
         count = 0
-        # for x in range(23):
-        #     button = FlagButton("Flag name here " + str(x), "Hi" + str(x))
-        #     button.setCheckable(True)
-        #     button.setToolTip(button.value)
-        #     button.clicked.connect(lambda checked, temp=button.value: self.buttonClicked(temp))
-        #     flagGrid.addWidget(button, count / 3, count % 3)
-        #     count += 1
-
-        for x in range(23):
-            cbox = FlagCheckBox("Flag name here " + str(x), "Hi" + str(x))
+        for flagname, flagdesc in self.simple.items():
+            cbox = FlagCheckBox(flagname, flagname)
             # cbox.setStyleSheet("font:bold; font-size:14px")
             cbox.setCheckable(True)
-            cbox.setToolTip(cbox.value)
-            cbox.clicked.connect(lambda checked, temp=cbox.value: self.buttonClicked(temp))
+            cbox.setToolTip(flagdesc['Explanation'])
+            cbox.clicked.connect(lambda checked, temp=cbox.value: self.flagButtonClicked("simple", temp))
             flagGrid.addWidget(cbox, count / 3, count % 3)
             count += 1
 
         tab1.setLayout(flagGrid)
+
+        # ----------- tab2 -------------------------
+        flagGrid = QGridLayout()
+        count = 0
+        for flagname, flagdesc in self.aesthetic.items():
+            cbox = FlagCheckBox(flagname, flagname)
+            # cbox.setStyleSheet("font:bold; font-size:14px")
+            cbox.setCheckable(True)
+            cbox.setToolTip(flagdesc['Explanation'])
+            cbox.clicked.connect(lambda checked, temp=cbox.value: self.flagButtonClicked("aesthetic", temp))
+            flagGrid.addWidget(cbox, count / 3, count % 3)
+            count += 1
+
+        tab2.setLayout(flagGrid)
+        # ----------- tab3 -------------------------
+        flagGrid = QGridLayout()
+        count = 0
+        for flagname, flagdesc in self.major.items():
+            cbox = FlagCheckBox(flagname, flagname)
+            # cbox.setStyleSheet("font:bold; font-size:14px")
+            cbox.setCheckable(True)
+            cbox.setToolTip(flagdesc['Explanation'])
+            cbox.clicked.connect(lambda checked, temp=cbox.value: self.flagButtonClicked("major", temp))
+            flagGrid.addWidget(cbox, count / 3, count % 3)
+            count += 1
+
+        tab3.setLayout(flagGrid)
+        # ----------- tab4 -------------------------
+        flagGrid = QGridLayout()
+        count = 0
+        for flagname, flagdesc in self.minor.items():
+            cbox = FlagCheckBox(flagname, flagname)
+            # cbox.setStyleSheet("font:bold; font-size:14px")
+            cbox.setCheckable(True)
+            cbox.setToolTip(flagdesc['Explanation'])
+            cbox.clicked.connect(lambda checked, temp=cbox.value: self.flagButtonClicked("minor", temp))
+            flagGrid.addWidget(cbox, count / 3, count % 3)
+            count += 1
+
+        tab4.setLayout(flagGrid)
+        # ----------- tab5 -------------------------
+        flagGrid = QGridLayout()
+        count = 0
+        for flagname, flagdesc in self.experimental.items():
+            cbox = FlagCheckBox(flagname, flagname)
+            # cbox.setStyleSheet("font:bold; font-size:14px")
+            cbox.setCheckable(True)
+            cbox.setToolTip(flagdesc['Explanation'])
+            cbox.clicked.connect(lambda checked, temp=cbox.value: self.flagButtonClicked("experimental", temp))
+            flagGrid.addWidget(cbox, count / 3, count % 3)
+            count += 1
+
+        tab5.setLayout(flagGrid)
+        # ----------- tab6 -------------------------
+        flagGrid = QGridLayout()
+        count = 0
+        for flagname, flagdesc in self.gamebreaking.items():
+            cbox = FlagCheckBox(flagname, flagname)
+            # cbox.setStyleSheet("font:bold; font-size:14px")
+            cbox.setCheckable(True)
+            cbox.setToolTip(flagdesc['Explanation'])
+            cbox.clicked.connect(lambda checked, temp=cbox.value: self.flagButtonClicked("gamebreaking", temp))
+            flagGrid.addWidget(cbox, count / 3, count % 3)
+            count += 1
+
+        tab6.setLayout(flagGrid)
 
         # this is the line in the layout that displays the string of selected flags
         #   and the button to save those flags
@@ -215,13 +278,13 @@ class Window(QWidget):
 
         bottomHBox.addWidget(QLabel("Saved flag selection: "))
 
-        list = ["Select flag preset (optional)",
+        combolist = ["Select flag preset (optional)",
                 "Dark Slash's formula",
                 "Muppets's Garbage Brew",
                 "Cecil's Murder Seed",
                 "CecilBot Special"]
         comboBox = QComboBox()
-        for string in list:
+        for string in combolist:
             comboBox.addItem(string)
         bottomHBox.addWidget(comboBox)
 
@@ -238,16 +301,100 @@ class Window(QWidget):
         return bottomGroupBox
 
     # Function to test button click / lambda expression functionality
-    def buttonClicked(self, value):
-        print(str(value))
+    def flagButtonClicked(self, dictionary, value):
 
+        if dictionary == "simple":
+            if not self.simple[value]['checked']:
+                self.simple[value]['checked'] = True
+            else:
+                self.simple[value]['checked'] = False
+        elif dictionary == "aesthetic":
+            if not self.aesthetic[value]['checked']:
+                self.aesthetic[value]['checked'] = True
+            else:
+                self.aesthetic[value]['checked'] = False
+        elif dictionary == "major":
+            if not self.major[value]['checked']:
+                self.major[value]['checked'] = True
+            else:
+                self.major[value]['checked'] = False
+        elif dictionary == "minor":
+            if not self.minor[value]['checked']:
+                self.minor[value]['checked'] = True
+            else:
+                self.minor[value]['checked'] = False
+        elif dictionary == "experimental":
+            if not self.experimental[value]['checked']:
+                self.experimental[value]['checked'] = True
+            else:
+                self.experimental[value]['checked'] = False
+        elif dictionary == "gamebreaking":
+            if not self.gamebreaking[value]['checked']:
+                self.gamebreaking[value]['checked'] = True
+            else:
+                self.gamebreaking[value]['checked'] = False
+
+    # Opens file dialog to select rom file and assigns it to value in parent class
     def openFileChooser(self):
         file_path = QFileDialog.getOpenFileName(self, 'Open File', './', filter="All Files(*.*);;Text Files(*.txt)")
         if file_path[0]:
             print(str(file_path[0]))
 
-        # QtCore.QString(file_path)
+        # display file location in text input field
         self.romInput.setText(str(file_path[0]))
+
+    # opens text files containing code flags/descriptions and puts data into separate dictionaries
+    def loadCodes(self):
+        codeLists = ["Codes-Aesthetic.txt", "Codes-Experimental.txt", "Codes-Gamebreaking.txt",
+                     "Codes-Major.txt", "Codes-Minor.txt", "Codes-Simple.txt"]
+
+        for fileList in codeLists:
+            with open("gui-codes/" + fileList, 'r') as fl:
+                content = fl.readlines()
+
+                if fileList == "Codes-Aesthetic.txt":
+                    for l in content:
+                        temp = re.split('\t|\n', l)
+                        temp = list(filter(None, temp))
+                        self.aesthetic[temp[0]] = {'Explanation': temp[1], 'checked': False}
+
+                elif fileList == "Codes-Experimental.txt":
+                    for l in content:
+                        temp = re.split('\t|\n', l)
+                        temp = list(filter(None, temp))
+                        # print(temp)
+                        self.experimental[temp[0]] = {'Explanation': temp[1], 'checked': False}
+
+                elif fileList == "Codes-Gamebreaking.txt":
+                    for l in content:
+                        temp = re.split('\t|\n', l)
+                        temp = list(filter(None, temp))
+                        self.gamebreaking[temp[0]] = {'Explanation': temp[1], 'checked': False}
+
+                elif fileList == "Codes-Major.txt":
+                    for l in content:
+                        temp = re.split('\t|\n', l)
+                        temp = list(filter(None, temp))
+                        self.major[temp[0]] = {'Explanation': temp[1], 'checked': False}
+
+                elif fileList == "Codes-Minor.txt":
+                    for l in content:
+                        temp = re.split('\t|\n', l)
+                        temp = list(filter(None, temp))
+                        self.minor[temp[0]] = {'Explanation': temp[1], 'checked': False}
+
+                elif fileList == "Codes-Simple.txt":
+                    for l in content:
+                        temp = re.split('\t|\n', l)
+                        temp = list(filter(None, temp))
+                        self.simple[temp[0]] = {'Explanation': temp[1], 'checked': False}
+
+        # print(self.aesthetic)
+        # print(self.experimental)
+        # print(self.gamebreaking)
+        # print(self.major)
+        # print(self.minor)
+        # print(self.simple)
 
 
 if __name__ == "__main__":
